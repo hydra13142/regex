6036ebf60302d75a13bb52b6ecba8f848fad8f71
hydra13142 <snake_dragon@126.com>
2013-10-17 21:03:09 +0800

    添加几个例子

---
+++ example/recur.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hydra13142/regex"
+)
+
+func main() {
+	reg := regex.Compile(`\s+|[-+*/]|\d+(?:\.\d+)?(?:[eE][-+]?\d+)?|\(#{0}+?\)`)
+	for _, u := range reg.FindAllString("1+(2-3)*(4/5.33)*((6-7)+8)") {
+		for _, v := range u {
+			fmt.Print(v, " ")
+		}
+		fmt.Println("")
+	}
+}
