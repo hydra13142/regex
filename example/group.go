c9d38265de5145a20cc04c9579a890afd418965b
liuzhixi <liudiwu@gmail.com>
2015-02-21 10:36:53 +0800

    Signed-off-by: liuzhixi <liudiwu@gmail.com>

--- example/group.go
+++ example/group.go
@@ -8,10 +8,6 @@ import (
 func main() {
 	reg := regex.Compile(`(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})(?::(\d{1,5}))?`)
 	for _, s := range reg.FindAllString("127.0.0.1:8087 255.33.1.2:12345  217.169.209.2:6666  192.227.139.106:7808") {
-		fmt.Printf("%-24s", s[0])
-		for _, x := range s[1:] {
-			fmt.Printf("%-8s", x)
-		}
-		fmt.Println("")
+		fmt.Printf("%-24s\n", s[0])
 	}
 }
--- example/recur.go
+++ example/recur.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	reg := regex.Compile(`\s+|[-+*/]|\d+(?:\.\d+)?(?:[eE][-+]?\d+)?|\(#{0}+?\)`)
-	for _, u := range reg.FindAllString("1+(2-3)*(4/5.33)*((6-7)+8)") {
+	for _, u := range reg.FindAllStringSubmatch("1+(2-3)*(4/5.33)*((6-7)+8)") {
 		for _, v := range u {
 			fmt.Print(v, " ")
 		}
